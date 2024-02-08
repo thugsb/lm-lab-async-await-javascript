2349c03ea40a666e1d0497f6f57a8b678562deca
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-02-08 15:20:42 +0000

    Get info about a cat

--- exercise002.js
+++ exercise002.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonTypicode =
+  "https://cat-fact.herokuapp.com/facts/58e00b5f0aac31001185ed24";
 
 const fetchData = async (apiEndPoint) => {
   try {
