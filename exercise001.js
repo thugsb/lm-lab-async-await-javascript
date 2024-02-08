dc7f0492b0d912ab8ab52f9c65247aee8e1ba342
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-02-08 14:53:36 +0000

    Feat: Handle promise to output appropriate text

--- exercise001.js
+++ exercise001.js
@@ -1,14 +1,21 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  // Generate random number between 0 and 9
+  const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
+  if (randomInt % 2 === 0) {
+    // Success
+    setTimeout(() => resolve("done"), 2000);
+  } else {
+    // Failure
+    setTimeout(() => reject("error"), 2000);
+  }
 });
 
 // Your solution(s) to exercise001 here!
+promise
+  .then((response) =>
+    console.log("Yay! Promise resolved with response: " + response)
+  )
+  .catch((response) =>
+    console.log("Boo. Promise rejected with response: " + response)
+  );
